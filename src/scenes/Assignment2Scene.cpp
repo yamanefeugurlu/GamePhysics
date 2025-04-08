32c2a366bbe907585d860d0b8b7d9e1afe91b6af
yamanefeugurlu <yamanefeugurlu@gmail.com>
2025-04-08 10:46:58 +0200

    created a new scene "Assignment 2"

---
+++ src/scenes/Assignment2Scene.cpp
@@ -0,0 +1,16 @@
+#include "Assignment2Scene.h"
+
+#include "imgui.h"
+
+void Assignment2Scene::OnEnable() {}
+
+void Assignment2Scene::OnDisable() {}
+
+void Assignment2Scene::Update(float deltaTime) {}
+
+void Assignment2Scene::Draw() {}
+
+void Assignment2Scene::DrawGUI() {
+    ImGui::Begin("Inspector");
+    ImGui::End();
+}
