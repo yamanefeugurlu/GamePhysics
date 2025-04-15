d235be027fa0c2146dc56c88c2f56c43d41352cb
yamanefeugurlu <yamanefeugurlu@gmail.com>
2025-04-15 12:13:20 +0200

    added Assignment 4 scene

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include "scenes/Assignment1Scene.h"
 #include "scenes//Assignment2Scene.h"
 #include "scenes/Assignment3.h"
+#include "scenes/Assignment4.h"
 
 int main(int argc, char** argv) {
     // Add newly created scenes here so that they show up in the menu.
     std::vector<Scene*> scenes = {new TestScene(), new Assignment1Scene(),
-                                  new Assignment2Scene(), new Assignment3()
+                                  new Assignment2Scene(), new Assignment3(),
+                                  new Assignment4()
     };
     Application app(scenes);
     return app.Run();
---
+++ src/scenes/Assignment4.cpp
@@ -0,0 +1,20 @@
+#include "Assignment4.h"
+
+#include "imgui.h"
+#include "../objects/Circle.h"
+#include "core/Colors.h"
+#include"core/Input.h"
+#include <vector> 
+
+void Assignment4::OnEnable() {}
+
+void Assignment4::OnDisable() {}
+
+void Assignment4::Update(float deltaTime) {}
+
+void Assignment4::Draw() {}
+
+void Assignment4::DrawGUI() {
+    ImGui::Begin("Inspector");
+    ImGui::End();
+}
